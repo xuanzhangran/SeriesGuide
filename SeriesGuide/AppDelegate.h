2e2f6fa5be95c247031cdd5dca3bc40f0f411276
xuan <1107160410@qq.com>
2018-06-27 14:06:35 +0800

    1.初始化App工程

---
+++ SeriesGuide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeriesGuide
+//
+//  Created by xuan on 2018/6/27.
+//  Copyright © 2018年 duotin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SeriesGuide/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SeriesGuide
+//
+//  Created by xuan on 2018/6/27.
+//  Copyright © 2018年 duotin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
